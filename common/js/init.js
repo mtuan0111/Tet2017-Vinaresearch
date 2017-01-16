331d6c02b24a53c1bdf146a4e90b54b3a4f57fa9
BOMNguyen <tuannguyen@vinaresearch.net>
2017-01-16 18:06:21 +0700

    [Update]

---
+++ common/js/init.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    var wheel = new Wheel();
+    // wheel.rotate();
+})
---
+++ common/js/wheelObs.js
@@ -0,0 +1,5 @@
+function Wheel(){}
+
+Wheel.prototype.rotate = function(){
+    alert(123);
+}
