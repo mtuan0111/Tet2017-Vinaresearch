30e5e3989252ac75dc408a17381e91251bf462a0
BOMNguyen <tuannguyen@vinaresearch.net>
2017-01-17 10:55:22 +0700

    [UPLoad PCoffice]

--- common/js/init.js
+++ common/js/init.js
@@ -2,3 +2,9 @@ $(document).ready(function(){
     var wheel = new Wheel();
     // wheel.rotate();
 })
+
+$(document).on("click",".bottomMenu a",function(){
+    // console.log($(this).data('page-target'));
+    var page_target = $(this).data('page-target')
+    $("#circle-world").attr("class",page_target);
+})
