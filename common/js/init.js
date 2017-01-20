33adec323e9cbf8b54d8e889b55ea67adf4e5620
BOMNguyen <tuannguyen@vinaresearch.net>
2017-01-20 21:14:40 +0700

    [Update]

--- common/js/init.js
+++ common/js/init.js
@@ -1,18 +1,31 @@
+$(document).ready(function(){
 
-$(window).load(function(){
-    // alert(123);
-    var wheel = new Wheel();
-    wheel.clickToPlay();
+    var hashPage = window.location.hash.split("#")[1]
+    if (hashPage){
+        var targetPage = $("#circle-world .page-" + hashPage)[0];
+        if(targetPage){
+            $("#circle-world").attr("class",hashPage);
+        };
+    }
 
-    var notiBroad = new notificationBroad($(".broad-obs.small"));
-    $(document).on("click",".loginButton",function(){
-        notiBroad.setLoginForm();
-    })
+    $(document).on("click",".bottomMenu a",function(){
+        // console.log($(this).data('page-target'));
+        var page_target = $(this).data('page-target')
+        $("#circle-world").attr("class",page_target);
+    });
+
 
 })
 
-$(document).on("click",".bottomMenu a",function(){
-    // console.log($(this).data('page-target'));
-    var page_target = $(this).data('page-target')
-    $("#circle-world").attr("class",page_target);
+document.ondrag = function(event) {
+    console.log(1);
+};
+
+$(window).load(function(){
+    var wheel = new Wheel();
+    wheel.clickToPlay();
+    var notiBroad = new notificationBroad($(".broad-obs.small"));
+    $(document).on("click",".loginButton",function(){
+        notiBroad.setLoginForm();
+    });
 })
